32bfbe86bb37fbf1a71a4515a94e0dd4c34cabf4
heowoong12 <81053210+heowoong12@users.noreply.github.com>
2023-08-24 23:14:47 -0700

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="header_logo" src="img/logo1.png" alt="로고" />
+    </div>
+  );
+}
