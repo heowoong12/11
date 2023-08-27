a6dd9a7406a51738343db3b63df3bd7259e80d46
heowoong12 <81053210+heowoong12@users.noreply.github.com>
2023-08-27 15:21:06 -0700

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <>
+      <span className="Statistic">금일 피해 신고 수</span>
+      <span className="Accumulate">누적 피해 신고 수</span>
+    </>
+  );
+}
---
+++ Main1.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Main1() {
+  return (
+    <>
+      <p>실시간 피해상황 공유</p>
+      <li>glkjla</li>
+      <li>kgljal</li>
+      <li>gla</li>
+      <li>gjkajhjkgas</li>
+      <li>kjglkasjglka</li>
+    </>
+  );
+}
